1db1f8639c0bb39d7775351e6e7f4dd912b4dcef
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-05 23:42:38 -0400

    "This is a getting easier with practice"

---
+++ src/demo4/Demo_Class_5.java
@@ -0,0 +1,11 @@
+package demo4;
+
+public class Demo_Class_5 {
+	
+	public static void main (String args []) {
+		
+		System.out.println("Demo Class 5");
+		System.out.println("Testing Demo Class 5");
+	}
+
+}
